22d832f1ca5ac8cbb2d4d8430f29d27e0a554039
Yashashwini Dixit <64366760+YashashwiniDixit@users.noreply.github.com>
2021-02-09 00:30:01 +0530

    views changed

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports =
 {
-    MongoURI:'mongodb+srv://yashashwinidixit:abc@123@cluster0.thuir.mongodb.net/user?retryWrites=true&w=majority'
+    MongoURI:'mongodb+srv://yashashwinidixit:abc@123@cluster0.thuir.mongodb.net/User?retryWrites=true&w=majority'
 }
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,45 @@ router.get('/login',(req,res)=> res.render('Login'));
 //register page
 router.get('/register',(req,res)=> res.render('Register'));
 
+//Register Handle
+router.post('/register',(req,res) => {
+    const{name,email,password,password2 } = req.body;
+    let errors =[];
+
+    //check required fields
+
+    if(!name || !email || !password || !password2 )
+    {
+        errors.push({ msg: 'Please fill in all fields'});
+    }
+
+    //check passwords match
+    if(password !== password2)
+    {
+        errors.push({mag: 'Passwords do not match'});
+    }
+
+    //check password length
+    if(password.length < 6)
+    {
+        errors.push({msg: 'Password should be atleast 6 characters long'});
+    }
+
+    if(errors.length >0)
+    {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    }
+    else
+    {
+        res.send=('pass');
+    }
+
+});
+
 module.exports = router;
